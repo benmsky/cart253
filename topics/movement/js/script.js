45845f5c38b7bb6b55c6367f17afeb33b1340a9e
Ben <benmacknofsky@gmail.com>
2024-09-16 21:31:32 -0400

    added a proper model of velocity and acceleration to the program

--- topics/movement/js/script.js
+++ topics/movement/js/script.js
@@ -1,24 +1,62 @@
 /**
- * Title of Project
- * Author Name
+ * Movement
+ * Benjamin Macknofsky
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Experimenting with movement
  */
 
 "use strict";
 
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50,
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    minVelocity: {
+        x: -3,
+        y: -2
+    },
+    maxVelocity: {
+        x: 3,
+        y: 2
+    },
+    acceleration: {
+        x: 0.025,
+        y: -0.2
+    }
+}
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Move the bird and display it
 */
 function draw() {
+    background(0);
 
+    //move the bird
+    bird.velocity.x += bird.acceleration.x;
+    bird.velocity.y += bird.acceleration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    bird.x += bird.velocity.x;
+    bird.y += bird.velocity.y;
+
+    //draw the bird
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size);
+    pop();
 }
